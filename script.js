95a2ea34643e38524e77653717e74fb90d75e519
David Lgc <legouellecd@gmail.com>
2022-08-30 16:53:06 +0200

    derniere vérification fonctionnalité + semantique

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 alert('Ceci est mon premier site, soyez indulgent' )
 
 
-
-const btn = document.querySelector("button.event")
-const texte = document.getElementById("paragraphe")
-function afficherNom() {
-  
-  texte.innerHTML = "John"
-}
-btn.addEventListener("click", afficherNom)
-
+// /* //////////////////CAROUSEL ////////////// */
